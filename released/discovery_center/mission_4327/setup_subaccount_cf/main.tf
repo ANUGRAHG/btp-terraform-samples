7743fea22fb9dd3d9551ff93a5b22737fbf3c435
Anugrah Gupta <anugrah.gupta@sap.com>
2024-03-13 11:52:21 +0530

    Update main.tf

--- released/discovery_center/mission_4327/setup_subaccount_cf/main.tf
+++ released/discovery_center/mission_4327/setup_subaccount_cf/main.tf
@@ -23,3 +23,94 @@ resource "btp_subaccount_role_collection_assignment" "subaccount-admins" {
   role_collection_name = "Subaccount Administrator"
   user_name            = each.value
 }
+######################################################################
+# Creation of Cloud Foundry environment
+######################################################################
+module "cloudfoundry_environment" {
+  source                    = "../../../modules/environment/cloudfoundry/envinstance_cf"
+  subaccount_id             = btp_subaccount.project.id
+  instance_name             = local.project_subaccount_cf_org
+  plan_name                 = "standard"
+  cf_org_name               = local.project_subaccount_cf_org
+  cf_org_auditors           = var.cf_org_auditors
+  cf_org_managers           = var.cf_org_managers
+  cf_org_billing_managers   = var.cf_org_billing_managers
+}
+######################################################################
+# Add "sleep" resource for generic purposes
+######################################################################
+resource "time_sleep" "wait_a_few_seconds" {
+  create_duration = "30s"
+}
+
+######################################################################
+# Add entitlement for BAS, Subscribe BAS and add roles
+######################################################################
+resource "btp_subaccount_entitlement" "bas" {
+  subaccount_id = btp_subaccount.project.id
+  service_name  = "sapappstudio"
+  plan_name     = var.bas_plan_name
+}
+resource "btp_subaccount_subscription" "bas-subscribe" {
+  subaccount_id = btp_subaccount.project.id
+  app_name      = "sapappstudio"
+  plan_name     = var.bas_plan_name
+  depends_on    = [btp_subaccount_entitlement.bas]
+}
+resource "btp_subaccount_role_collection_assignment" "Business_Application_Studio_Administrator" {
+  subaccount_id        = btp_subaccount.project.id
+  role_collection_name = "Business_Application_Studio_Administrator"
+  user_name            = var.btp_user
+  depends_on           = [btp_subaccount_subscription.bas-subscribe]
+}
+resource "btp_subaccount_role_collection_assignment" "Business_Application_Studio_Developer" {
+  subaccount_id        = btp_subaccount.project.id
+  role_collection_name = "Business_Application_Studio_Developer"
+  user_name            = var.btp_user
+  depends_on           = [btp_subaccount_subscription.bas-subscribe]
+}
+######################################################################
+# Add Build Workzone entitlement subscription and role Assignment
+######################################################################
+resource "btp_subaccount_entitlement" "build_workzone" {
+  subaccount_id = btp_subaccount.project.id
+  service_name  = "SAPLaunchpad"
+  plan_name     = var.build_workzone_plan_name
+}
+resource "btp_subaccount_subscription" "build_workzone_subscribe" {
+  subaccount_id = btp_subaccount.project.id
+  app_name      = "SAPLaunchpad"
+  plan_name     = var.build_workzone_plan_name
+  depends_on    = [btp_subaccount_entitlement.build_workzone]
+}
+resource "btp_subaccount_role_collection_assignment" "launchpad_admin" {
+  subaccount_id        = btp_subaccount.project.id
+  role_collection_name = "Launchpad_Admin"
+  user_name            = var.btp_user
+  depends_on           = [btp_subaccount_subscription.build_workzone_subscribe]
+}
+######################################################################
+# Create HANA entitlement subscription
+######################################################################
+resource "btp_subaccount_entitlement" "hana-cloud" {
+  subaccount_id    = btp_subaccount.project.id
+  service_name     = "hana-cloud"
+  plan_name        = var.hana-cloud_plan_name
+}
+# Enable HANA Cloud Tools
+resource "btp_subaccount_entitlement" "hana-cloud-tools" {
+  subaccount_id    = btp_subaccount.project.id
+  service_name     = "hana-cloud-tools"
+  plan_name        = "tools"
+}
+resource "btp_subaccount_subscription" "hana-cloud-tools" {
+  subaccount_id    = btp_subaccount.project.id
+  app_name         = "hana-cloud-tools"
+  plan_name        = "tools"
+  depends_on       = [btp_subaccount_entitlement.hana-cloud-tools]
+}
+resource "btp_subaccount_entitlement" "hana-hdi-shared" {
+  subaccount_id    = btp_subaccount.project.id
+  service_name     = "hana"
+  plan_name        = "hdi-shared"
+}
