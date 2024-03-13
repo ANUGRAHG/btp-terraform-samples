5b41a0239d16f3badccdaa12e38baa569e6eb2d7
Anugrah Gupta <anugrah.gupta@sap.com>
2024-03-13 13:38:45 +0530

    Update variables.tf

--- released/discovery_center/mission_4327/setup_subaccount_cf/variables.tf
+++ released/discovery_center/mission_4327/setup_subaccount_cf/variables.tf
@@ -22,7 +22,7 @@ variable "region" {
 variable "cli_server_url" {
   type        = string
   description = "The BTP CLI server URL."
-  default     = "https://cpcli.cf.eu10.hana.ondemand.com"
+  default     = "https://cpcli.cf.sap.hana.ondemand.com"
 }
 
 variable "subaccount_admins" {
