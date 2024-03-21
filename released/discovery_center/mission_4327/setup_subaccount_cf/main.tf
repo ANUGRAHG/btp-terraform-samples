56ea12a7564a140ae3444b556f187740fd36a1b7
Anugrah Gupta <anugrah.gupta@sap.com>
2024-03-21 13:11:00 +0530

    Update main.tf

--- released/discovery_center/mission_4327/setup_subaccount_cf/main.tf
+++ released/discovery_center/mission_4327/setup_subaccount_cf/main.tf
@@ -23,3 +23,61 @@ resource "btp_subaccount_role_collection_assignment" "subaccount-admins" {
   role_collection_name = "Subaccount Administrator"
   user_name            = each.value
 }
+######################################################################
+# Creation of Cloud Foundry environment
+######################################################################
+resource "btp_subaccount_environment_instance" "cloudfoundry" {
+  subaccount_id    = btp_subaccount.project.id
+  name             = "my-cf-environment"
+  landscape_label  = "cf-eu10"
+  environment_type = "cloudfoundry"
+  service_name     = "cloudfoundry"
+  plan_name        = "standard"
+  # ATTENTION: some regions offer multiple environments of a kind and you must explicitly select the target environment in which
+  # the instance shall be created using the parameter landscape label. 
+  # available environments can be looked up using the btp_subaccount_environments datasource
+  parameters = jsonencode({
+    instance_name = "my-cf-org-name"
+  })
+  timeouts = {
+    create = "1h"
+    update = "35m"
+    delete = "30m"
+  }
+}
+
+######################################################################
+# Add "sleep" resource for generic purposes
+######################################################################
+resource "time_sleep" "wait_a_few_seconds" {
+  create_duration = "30s"
+}
+
+######################################################################
+# Add entitlement for BAS, Subscribe BAS and add roles
+######################################################################
+resource "btp_subaccount_entitlement" "bas" {
+  subaccount_id = btp_subaccount.project.id
+  service_name  = "sapappstudio"
+  plan_name     = var.bas_plan_name
+}
+resource "btp_subaccount_subscription" "bas-subscribe" {
+  subaccount_id = btp_subaccount.project.id
+  app_name      = "sapappstudio"
+  plan_name     = var.bas_plan_name
+  depends_on    = [btp_subaccount_entitlement.bas]
+}
+resource "btp_subaccount_role_collection_assignment" "Business_Application_Studio_Administrator" {
+  subaccount_id        = btp_subaccount.project.id
+  role_collection_name = "Business_Application_Studio_Administrator"
+  user_name            = data.btp_whoami.me.email
+  depends_on           = [btp_subaccount_subscription.bas-subscribe]
+}
+
+
+resource "btp_subaccount_role_collection_assignment" "Business_Application_Studio_Developer" {
+  subaccount_id        = btp_subaccount.project.id
+  role_collection_name = "Business_Application_Studio_Developer"
+  user_name            = data.btp_whoami.me.email
+  depends_on           = [btp_subaccount_subscription.bas-subscribe]
+}
